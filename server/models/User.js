4f303ecf6990cee1462bb6578098910ba7e46a38
Arqam Souban <arqamsouban55@gmail.com>
2025-10-03 23:55:36 +0500

    fix

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["employer", "applicant"], default: "applicant" },
+  profilePic: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
 
