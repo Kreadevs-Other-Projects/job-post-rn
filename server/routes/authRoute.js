279734cc17bfc8f58c0bee2b778d973fc5a8cd51
Arqam Souban <arqamsouban55@gmail.com>
2025-10-04 00:39:15 +0500

    fix

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../middleware/upload");
 const { register, login } = require("../controllers/authController");
 
 router.post("/register", upload.single("profilePic"), register);
